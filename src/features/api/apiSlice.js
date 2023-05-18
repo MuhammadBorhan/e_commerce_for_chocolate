e568385fbae1f16fcd6f7367cbc7c85a753a7713
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-05-18 11:22:38 +0600

    so far so good

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,7 +5,7 @@ const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:5000/api/v1",
   }),
-  tagTypes: ["regions", "login", "product"],
+  tagTypes: ["regions", "login", "product", "trend"],
   endpoints: (builder) => ({}),
 });
 
