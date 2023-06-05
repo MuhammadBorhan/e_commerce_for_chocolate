dd6cd33e726cdee41873d03804c42371dde68d03
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-06-05 16:21:17 +0600

    all is ok

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,6 +8,13 @@ import Regions from "../Components/Regions";
 import Footer from "../Layout/Footer";
 
 const Home = () => {
+  const regions = [
+    { region: "Dhaka", district: ["mohammadpur", "rampura", "badda"] },
+    { region: "Comilla", district: ["laksham", "lalmai", "barura", "sadar"] },
+    { region: "Chattagram", district: ["agrabad", "hamjarbag", "muradpur"] },
+  ];
+  const [selectedRegion, setSelectedRegion] = useState(null);
+
   return (
     <>
       <MobileSearch />
@@ -17,6 +24,41 @@ const Home = () => {
       <Regions />
       <SameDayDelivery />
       <Footer />
+
+      <div className="p-20">
+        {selectedRegion ? (
+          <button
+            onClick={() => setSelectedRegion(null)}
+            className="border-2 rounded-full px-2"
+          >
+            {selectedRegion?.region} &#8595;
+          </button>
+        ) : (
+          <div>
+            <span>Choose Region</span>
+            <div className="flex gap-x-12">
+              {regions.map((region) => (
+                <button onClick={() => setSelectedRegion(region)}>
+                  {region?.region}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+
+        <div>
+          {selectedRegion && (
+            <div>
+              <h4 className=" font-bold my-2">Choose District</h4>
+              <div className="flex gap-4 mx-auto ">
+                {selectedRegion?.district?.map((dis) => (
+                  <button className="border py-1 px-2">{dis}</button>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
 };
