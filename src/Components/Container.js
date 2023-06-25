031f0cf84f397d92cbbe2841e215948d26485f64
alamin_saifur <saifurrahman1614@gmail.com>
2023-06-26 01:07:47 +0600

    accepted

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-
-const Container = ({children}) => {
-    return (
-        <div className='w-full max-w-[1280px] mx-auto px-6 md:px-auto'>
-            {children}
-        </div>
-    );
-};
-
-export default Container;
-=======
 import React from "react";
 
 const Container = ({ children }) => {
@@ -22,4 +9,3 @@ const Container = ({ children }) => {
 };
 
 export default Container;
->>>>>>> 010fffab51dc34ab2d95bacc5a5d03a0f7d5f621
--- src/Pages/Dashboard/AllUsers/SubscriptionUser.js
+++ src/Pages/Dashboard/AllUsers/SubscriptionUser.js
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import DashBoardMenu from '../../../Components/DashBoardMenu/DashBoardMenu';
-const SubscriptionUser = () => {
-//     const [subsUsers, setSubsUsers] = useState([]);
-//    useEffect(()=>{
-//     fetch("subscription.json")
-//     .then((res)=>res.json())
-//     .then((data)=>console.log(data))
-//    },[])
-const subsUsers = [
-    {
-      "_id": 1,
-      "packageName": "Basic Level",
-      "type":"Free",
-      "price": "Free"
-    },
-    {
-      "_id": 2,
-      "packageName": "Weekly",
-      "type":"Payment",
-      "price": "¥30"
-    },
-    {
-      "_id": 3,
-      "packageName": "Monthly",
-      "type":"Paymnet",
-      "price": "¥100"
-    },
-    {
-      "_id": 4,
-      "packageName": "Yearly",
-      "type":"Payment",
-      "price": "¥300"
-    }
-    
-  ]
-
-    return (
-        <div>
-        <DashBoardMenu></DashBoardMenu>
-        <div className="p-8">
-          <div className="overflow-x-auto">
-            <h2 className="text-xl font-bold mt-2">All User</h2>
-            <table className="table w-full mt-2">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th>Sl No.</th>
-                  <th>Package Name</th>
-                  <th>Type</th>
-                  <th>Payment</th>
-                  <th>User Count</th>
-                  {/* <th>Action</th> */}
-                </tr>
-              </thead>
-              <tbody>
-                {subsUsers?.map((user, i) => (
-                  <tr key={user._id}>
-                    <th>{i + 1}</th>
-                    <td>{user.packageName}</td>
-                    <td>{user.type}</td>
-                    <td>{user.price}</td>
-                    <td>2</td>
-                    {/* <td>
-=======
 import React from "react";
 import DashBoardMenu from "../../../Components/DashBoardMenu/DashBoardMenu";
 const SubscriptionUser = () => {
@@ -127,7 +61,6 @@ const SubscriptionUser = () => {
                   <td>{user.price}</td>
                   <td>2</td>
                   {/* <td>
->>>>>>> 010fffab51dc34ab2d95bacc5a5d03a0f7d5f621
                       {" "}
                       <button
                         className="text-red-500 flex justify-center"
@@ -136,20 +69,6 @@ const SubscriptionUser = () => {
                         <AiTwotoneDelete></AiTwotoneDelete>
                       </button>
                     </td> */}
-<<<<<<< HEAD
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <button className='btn bg-yellow-900'>Add New</button>
-        </div>
-      </div>
-    );
-};
-
-export default SubscriptionUser;
-=======
                 </tr>
               ))}
             </tbody>
@@ -162,4 +81,3 @@ export default SubscriptionUser;
 };
 
 export default SubscriptionUser;
->>>>>>> 010fffab51dc34ab2d95bacc5a5d03a0f7d5f621
